3fd032813b4b93998f0709ade194f22794745822
Indistoupia <j.nolan@alumnos.santotomas.cl>
2023-10-03 23:50:27 -0300

    Se han quitado clases no necesarias

--- app/src/main/java/com/hola/evalua/MyData.java
+++
@@ -1,9 +0,0 @@
-package com.hola.evalua;
-
-public class MyData {
-    static String[] nameArray = {"Cupcake", "Bonita", "Hermosa", "Preciosa", "Linda", "HolaSoyLaKikyo"};
-
-    static Integer[] drawableArray = {R.drawable.Acostadita, R.drawable.AcostadaEnMiBracito, R.drawable.RecostadaComoQueque,
-            R.drawable.MirandoHermosamente, R.drawable.PidiendoComida, R.drawable.Relajada};
-    static Integer[] id_ = {0, 1, 2, 3, 4};
-}
